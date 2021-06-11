731e1affc642745762c852730a065f3391e5252b
amarizzal <rizal.ammar91@yahoo.com>
2021-06-11 09:00:37 +0700

    Update about us

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,6 +4,10 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="theme-color" content="#0099ff">
+
+    <!-- favicon -->
+    <link rel="shortcut icon" type="image/jpg" href="assets/logo-only-ospam.png"/>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
@@ -14,58 +18,91 @@
     <title>Muara Cita</title>
   </head>
   <body>
-    <nav class="navbar navbar-light" style="background-color: #0099ff;">
+    <nav class="navbar fixed-top navbar-light">
         <div class="container nav">
-            <a class="navbar-brand" style="color: #007fd8;" href="#">
-                <img src="assets/logo-ospam.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
+            <a class="navbar-brand" style="color: #000B76;" href="#">
+                <img src="assets/logo-only-ospam.png" alt="" width="30" height="30" class="d-inline-block align-text-top">
                 Muara Cita
             </a>
             <ul class="nav justify-content-center">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Active</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#jumbotron">Logo</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#visimisi">Visi Misi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Anggota</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">App</a>
+                </li>
             </ul>
             <button class="btn btn-dark" type="submit">Search</button>
         </div>
     </nav>
 
-    <section class="jumbotron">
-        <div class="jumbotron-wrapper" style="background-color: #0099ff;">
+    <!-- ============================ JUMBOTRON ============================== -->
+    <section class="jumbotron" id="jumbotron">
+        <div class="jumbotron-wrapper pt-5" style="background-color: #0099ff;">
             <div class="container">
-                <div class="row align-items-center">
+                <div class="row align-items-center justify-content-center py-5">
                     <div class="col">
                         <!-- <h1 class="jumbotron-title">OSPAM</h1> -->
-                        <h1 class="jumbotron-title">Muara Cita</h1>
+                        <h1 class="title">Muara Cita</h1>
                         <p class="jumbotron-text">Sinergi Bersama Santri</p>
                     </div>
-                    <div class="col">
-                        <img src="https://res.cloudinary.com/dklmpixki/image/upload/v1623169542/Logo_OSPAM_Muara_Cita_1_tcylmb.png" alt="" width=200>
+                    <div class="col row just    ify-content-center p-6">
+                        <div class="img-wrapper text-center">
+                            <img src="assets/logo-only-ospam.png" alt="" height=400 class="m-auto">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#0099ff" fill-opacity="1" d="M0,64L60,96C120,128,240,192,360,224C480,256,600,256,720,245.3C840,235,960,213,1080,176C1200,139,1320,85,1380,58.7L1440,32L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path></svg>
         <!-- <svg id="wave" style="transform:rotate(180deg); transition: 0.3s" viewBox="0 0 1440 190" version="1.1" xmlns="http://www.w3.org/2000/svg"><defs><linearGradient id="sw-gradient-0" x1="0" x2="0" y1="1" y2="0"><stop stop-color="rgba(0, 153, 255, 1)" offset="0%"></stop><stop stop-color="rgba(0, 11, 118, 1)" offset="100%"></stop></linearGradient></defs><path style="transform:translate(0, 0px); opacity:1" fill="url(#sw-gradient-0)" d="M0,95L80,79.2C160,63,320,32,480,38C640,44,800,89,960,88.7C1120,89,1280,44,1440,41.2C1600,38,1760,76,1920,79.2C2080,82,2240,51,2400,50.7C2560,51,2720,82,2880,98.2C3040,114,3200,114,3360,123.5C3520,133,3680,152,3840,142.5C4000,133,4160,95,4320,79.2C4480,63,4640,70,4800,85.5C4960,101,5120,127,5280,136.2C5440,146,5600,139,5760,117.2C5920,95,6080,57,6240,44.3C6400,32,6560,44,6720,69.7C6880,95,7040,133,7200,136.2C7360,139,7520,108,7680,98.2C7840,89,8000,101,8160,95C8320,89,8480,63,8640,63.3C8800,63,8960,89,9120,101.3C9280,114,9440,114,9600,104.5C9760,95,9920,76,10080,76C10240,76,10400,95,10560,101.3C10720,108,10880,101,11040,107.7C11200,114,11360,133,11440,142.5L11520,152L11520,190L11440,190C11360,190,11200,190,11040,190C10880,190,10720,190,10560,190C10400,190,10240,190,10080,190C9920,190,9760,190,9600,190C9440,190,9280,190,9120,190C8960,190,8800,190,8640,190C8480,190,8320,190,8160,190C8000,190,7840,190,7680,190C7520,190,7360,190,7200,190C7040,190,6880,190,6720,190C6560,190,6400,190,6240,190C6080,190,5920,190,5760,190C5600,190,5440,190,5280,190C5120,190,4960,190,4800,190C4640,190,4480,190,4320,190C4160,190,4000,190,3840,190C3680,190,3520,190,3360,190C3200,190,3040,190,2880,190C2720,190,2560,190,2400,190C2240,190,2080,190,1920,190C1760,190,1600,190,1440,190C1280,190,1120,190,960,190C800,190,640,190,480,190C320,190,160,190,80,190L0,190Z"></path></svg> -->
-    </section>
-
+        </section>
+        
+        <!-- =========================== VISI MISI ============================= -->
+        <section class="visimisi container" id="visimisi">
+            <h1 class="title text-center">Kemana kita Bergerak?</h1>
+            <div class="row">
+                <div class="col">
+                    <img src="assets/logo-only-ospam.png" alt="" height=400 class="m-auto">
+                </div>
+                <div class="col">
+                    <h3 class="title-sec">Visi</h3>
+                    <p class="visimisi-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, doloribus?</p>
+                    <h3 class="title-sec">Misi</h3>
+                    <p class="visimisi-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, doloribus?</p>
+                </div>
+            </div>
+        </section>
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--s
+    <!--
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
     -->
+
+    <!-- ============================ OWN JAVASCRIPT ========================= -->
+    <script>
+        document.addEventListener('scroll', function(e) {
+            lastKnownScrollPosition = window.scrollY;
+            console.log(lastKnownScrollPosition);
+            const navbar = document.querySelector(".navbar");
+            if (lastKnownScrollPosition > 450) {
+                navbar.style.backgroundColor = "#0099ff";
+            } else {
+                navbar.style.backgroundColor = "transparent";
+
+            }
+        });
+    </script>
   </body>
 </html>
